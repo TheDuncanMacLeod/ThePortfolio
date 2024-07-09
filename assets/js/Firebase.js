d1920122a025a77ce1dce4e46efbf1baa03be07f
Duncan MacLeod <r.Gael01@gmail.com>
2024-07-09 17:01:54 +0200

    essaie 1 rectification problèmes JS

--- assets/js/Firebase.js
+++ assets/js/Firebase.js
@@ -1,52 +1,51 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDivJYowj8vS2bwIL-v_8ni137u_NbWaas",
+  authDomain: "testdb111-3cc4e.firebaseapp.com",
+  databaseURL: "https://testdb111-3cc4e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "testdb111-3cc4e",
+  storageBucket: "testdb111-3cc4e.appspot.com",
+  messagingSenderId: "194820217287",
+  appId: "1:194820217287:web:5e4e41997ce2ad2e9861e2",
+  measurementId: "G-89H5X1VHTD"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
-auth.signInAnonymously().catch(function(error) {
-console.error('Authentication error:', error);
-});
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBY-upsPz8tgMUzmdt-r4uB61zI8k8OqBs",
-    authDomain: "messagessite1.firebaseapp.com",
-    // The value of `databaseURL` depends on the location of the database
-    projectId: "messagessite1",
-    storageBucket: "messagessite1.appspot.com",
-    messagingSenderId: "971917162916",
-    appId: "1:971917162916:web:ce325acb1b95e833e9a931",
-    // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-    measurementId: "G-GXMNREKVQ1"
-  };
+const contactFormDB = ref(database, 'contactForm');
 
-const form = document.getElementById('contactForm'); 
 form.addEventListener('submit', (e) => {
-e.preventDefault(); // Empêche l'envoi par défaut du formulaire
+    e.preventDefault(); // Empêche l'envoi par défaut du formulaire
 
-// Récupération des valeurs du formulaire
-const name = form['name'].value;
-const email = form['email'].value;
-const subject = form['subject'].value;
-const message = form['message'].value;
-console.log("test booba")
+    // Récupération des valeurs du formulaire
+    const name = form['name'].value;
+    const email = form['email'].value;
+    const subject = form['subject'].value;
+    const message = form['message'].value;
 
-// Enregistrement des données dans Firestore
-const db = firebase.firestore();
-db.collection('contacts').add({
-    name: name,
-    email: email,
-    subject: subject,
-    message: message,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp() // Optionnel : timestamp de l'enregistrement
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-    // Réinitialisation du formulaire après soumission
-    form.reset();
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
+    // Enregistrement des données dans Realtime Database
+    set(contactFormDB, {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+        timestamp: firebase.database.ServerValue.TIMESTAMP // Optionnel : timestamp de l'enregistrement
+    })
+    .then(() => {
+        console.log("Data successfully written to Realtime Database");
+        // Réinitialisation du formulaire après soumission
+        form.reset();
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
+    });
 });
-});
-
 
