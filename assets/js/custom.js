1759d778264fe14ea0670f6d2aa47ed237ab8fb1
Duncan MacLeod <r.Gael01@gmail.com>
2024-07-09 15:35:37 +0200

    changement de place de la config firebase

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -124,6 +124,31 @@ $(document).ready(function(){
         });
 
 });	
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	const analytics = getAnalytics(app);
+
+	auth.signInAnonymously().catch(function(error) {
+    console.error('Authentication error:', error);
+});
+
+// Configuration de votre application Firebase
+const firebaseConfig = {
+	apiKey: "AIzaSyBY-upsPz8tgMUzmdt-r4uB61zI8k8OqBs",
+	authDomain: "messagessite1.firebaseapp.com",
+	projectId: "messagessite1",
+	storageBucket: "messagessite1.appspot.com",
+	messagingSenderId: "971917162916",
+	appId: "1:971917162916:web:ce325acb1b95e833e9a931",
+	measurementId: "G-GXMNREKVQ1"
+  };
+
+// Initialisation de Firebase
+const app = initializeApp(firebaseConfig);
+
+// Accès à Firestore (base de données)
+const db = getFirestore(app);
+
 	
 const form = document.getElementById('contactForm'); 
 form.addEventListener('submit', (e) => {
